5ae7fcdfa9030f86d6dcdcbbdc28c2f26f7ea0f1
Alex Miasnikov <alexm0109@gmail.com>
2013-07-12 11:03:54 -0400

    3 files fixed #lmprs-5 fixed tag to be tested

--- src/Main3.java
+++ src/Main3.java
@@ -1,6 +1,6 @@
 public class Main3 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World testing 3 !");
+        System.out.println("Hello World testing 03 !");
     }
 }
