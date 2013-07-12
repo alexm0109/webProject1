304895b3d5c42dd105c9001ae9801707aa57c866
Alex Miasnikov <alexm0109@gmail.com>
2013-07-12 10:19:31 -0400

    #lmprs-3 fixed tag to be tested

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World Testing 000 !");
+        System.out.println("Hello World Testing 001 !");
     }
 }
--- src/Main2.java
+++ src/Main2.java
@@ -1,6 +1,6 @@
 public class Main2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World Testing 222 !");
+        System.out.println("Hello World Testing 002 !");
     }
 }
