2883391288f7d76f18c32a5305be66d1963697c1
Alex Miasnikov <alexm0109@gmail.com>
2013-07-12 10:34:59 -0400

    #lmprs-4 fixed changed missing file again tag to be tested

--- src/Main3.java
+++ src/Main3.java
@@ -1,6 +1,6 @@
 public class Main3 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World Testing 333 !");
+        System.out.println("Hello World Testing 3 !");
     }
 }
