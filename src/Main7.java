1223c746e9d647ef09827f35054f90e8d7fb92af
Alex Miasnikov <amiasnikov@dminc.com>
2013-08-14 14:25:19 -0400

    #lmprs-7 editing file #7

--- src/Main7.java
+++ src/Main7.java
@@ -1,6 +1,6 @@
 public class Main7 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World testing 7 !");
+        System.out.println("Hello World testing 07 !");
     }
 }
