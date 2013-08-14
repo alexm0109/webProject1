138e577111a4a000d5bdf25ece7ae6af97f05268
alexm0109 <alexm0109@gmail.com>
2013-08-14 14:21:42 -0400

    #lmprs-6
    Just playing around

--- src/Main5.java
+++ src/Main5.java
@@ -1,6 +1,6 @@
 public class Main5 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World testing 005 !");
+        System.out.println("Hello World testing 5 !");
     }
 }
